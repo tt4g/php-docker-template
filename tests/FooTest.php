823b4acab5481fab29904a10ebc8f7b2107edc1c
tt4g <mainlinett@gmail.com>
2021-02-06 17:38:39 +0900

    Install PHPUnit

---
+++ tests/FooTest.php
@@ -0,0 +1,13 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+/**
+ * This class for checking the operation of PHPUnit command.
+ */
+class FooTest extends TestCase
+{
+    public function testFoo()
+    {
+        $this->assertTrue(TRUE);
+    }
+}
